2a5a2cecd99028ac4b5ec7bc19bf7e187d4d86c8
RafaSolorio <rafavsgc@gmail.com>
2022-06-23 21:24:58 -0700

    layout updated

--- weather/js/toggle.js
+++ weather/js/toggle.js
@@ -1,3 +1,11 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+function displayBanner() {
+    dayOfWeek = new Date().getDay();
+    if (dayOfWeek == 6) {
+        document.querySelector('#banner').style.display = "block";
+    }
+    console.log(dayOfWeek);
+}
