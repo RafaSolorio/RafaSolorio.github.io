a28920428881de9d7f8800beee7b2529afb03e7e
RafaSolorio <rafavsgc@gmail.com>
2022-06-27 22:49:23 -0700

    Updates

--- weather/js/windchill.js
+++ weather/js/windchill.js
@@ -2,12 +2,13 @@ function computeWindChill(temperature, windSpeed) {
     return 35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16;
 }
 
-let t = document.querySelector('#temperature').innerHTML;
-let ws = document.querySelector('#wind-speed').innerHTML;
+function windChill() {
+    let t = document.querySelector('#temperature').innerHTML;
+    let ws = document.querySelector('#wind-speed').innerHTML;
 
-if (t <= 50 && ws > 3.0) {
-    document.querySelector("#wind-chill").innerHTML = computeWindChill(temperature, windSpeed);
-}
-else {
-    document.querySelector('#windchill').innerHTML = "N/A";
+    if (t >= 50 && ws >= 3){
+        document.querySelector('#wind-chill').innerHTML = Math.round(computeWindChill(t, ws));
+    } else {
+        document.querySelector('#wind-chill').innerHTML = "N/A";
+    }
 }
