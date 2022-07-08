1802b600eacaa43aa365bde9512bbe866da2eea0
RafaSolorio <rafavsgc@gmail.com>
2022-07-07 23:15:03 -0700

    Home page updates

--- lesson-5/JS/towns.js
+++ lesson-5/JS/towns.js
@@ -22,22 +22,26 @@ fetch(requestURL)
               
                 mottoP.textContent = towns[i].motto;
                 mottoP.setAttribute('class', towns[i].name);
-                mottoP.setAttribute('id', 'mottoT');
+                mottoP.setAttribute('class', 'mottoT');
                 card.appendChild(mottoP);
               
                 yearP.textContent = 'Year Founded: ' + towns[i].yearFounded;
                 yearP.setAttribute('class', towns[i].name);
+                yearP.setAttribute('id', towns[i].name + 'year');
                 card.appendChild(yearP);
       
                 popP.textContent = 'Population: ' + towns[i].currentPopulation;
                 popP.setAttribute('class', towns[i].name);
+                popP.setAttribute('id', towns[i].name + 'pop');
                 card.appendChild(popP);
               
                 rainP.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
                 rainP.setAttribute('class', towns[i].name);
+                rainP.setAttribute('id', towns[i].name + 'rainfall');
                 card.appendChild(rainP);
               
-                image.setAttribute('src', towns[i].photo);
+                image.setAttribute('src', "https://via.placeholder.com/400x400");
+                image.setAttribute('data-src', towns[i].photo);
                 image.setAttribute('class', towns[i].name + 'img');
                 card.appendChild(image);
               
