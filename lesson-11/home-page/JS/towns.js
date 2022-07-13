a60bcf1f5e88fed8b2ca217c15655d1e396bbbaf
RafaSolorio <rafavsgc@gmail.com>
2022-07-13 00:35:24 -0700

    Lesson-11 folder completed

---
+++ lesson-11/home-page/JS/towns.js
@@ -0,0 +1,53 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+
+        for (let i = 0; i < towns.length; i++ ) {
+            if (towns[i].name == 'Preston' || towns[i].name == 'Soda Springs' || towns[i].name == 'Fish Haven') {
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let mottoP = document.createElement('p');
+                let yearP = document.createElement('p');
+                let popP = document.createElement('p');
+                let rainP = document.createElement('p');
+                let image = document.createElement('img');
+                h2.textContent = towns[i].name;
+                h2.setAttribute('class', towns[i].name);
+                card.appendChild(h2);
+              
+                mottoP.textContent = towns[i].motto;
+                mottoP.setAttribute('class', towns[i].name);
+                mottoP.setAttribute('class', 'mottoT');
+                card.appendChild(mottoP);
+              
+                yearP.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                yearP.setAttribute('class', towns[i].name);
+                yearP.setAttribute('id', towns[i].name + 'year');
+                card.appendChild(yearP);
+      
+                popP.textContent = 'Population: ' + towns[i].currentPopulation;
+                popP.setAttribute('class', towns[i].name);
+                popP.setAttribute('id', towns[i].name + 'pop');
+                card.appendChild(popP);
+              
+                rainP.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+                rainP.setAttribute('class', towns[i].name);
+                rainP.setAttribute('id', towns[i].name + 'rainfall');
+                card.appendChild(rainP);
+              
+                image.setAttribute('src', "https://via.placeholder.com/400x400");
+                image.setAttribute('data-src', towns[i].photo);
+                image.setAttribute('class', towns[i].name + 'img');
+                image.setAttribute('alt', towns[i].name);
+                card.appendChild(image);
+              
+                document.querySelector('div.towns').appendChild(card);
+            }
+        }
+        
+      });
---
+++ lesson-11/preston/js/weatherapi.js
@@ -0,0 +1,30 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=653115c2b12015a06e90d44cf9a1fe16&units=imperial';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById('currently').textContent = jsObject.weather[0].main;
+    document.getElementById('temperature').textContent = jsObject.main.temp_max;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('wind-speed').textContent = jsObject.wind.speed;
+  });
+
+const apiURLf = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=653115c2b12015a06e90d44cf9a1fe16&units=imperial';
+fetch(apiURLf)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const dayList = jsObject.list;
+
+    let index=0;
+    dayList.forEach(element => {
+        if (element.dt_txt.includes("18:00:00")){
+            const iconClass = document.getElementsByClassName('iconf');
+            const forecastClass = document.getElementsByClassName('forecast');
+            forecastClass[index].textContent = element.weather[0].main;
+            const imagesrc = 'https://openweathermap.org/img/w/' + element.weather[0].icon + '.png';
+            const desc = element.weather[0].description;
+            iconClass[index].setAttribute('src', imagesrc);  
+            iconClass[index].setAttribute('alt', desc);
+            index += 1;
+        }
+    });
+  });
